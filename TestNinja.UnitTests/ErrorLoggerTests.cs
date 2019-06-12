e2d045a23c5b0e512585bd28bfedb20421c263e8
Zach Allen <zachjallen@gmail.com>
2019-06-12 14:54:14 -0300

    added test for void method

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}

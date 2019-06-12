27a3a33aa4d17a7a19f5c8053b9d2a15d76a0bbd
Zach Allen <zachjallen@gmail.com>
2019-06-12 14:42:34 -0300

    added return object type tests

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var actual = controller.GetCustomer(0);
+
+            // Not Found
+            Assert.That(actual, Is.TypeOf<NotFound>());
+            // or
+            // NotFound or one of it's derivatives
+            //Assert.That(actual, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+
+            var actual = controller.GetCustomer(1);
+
+            Assert.That(actual, Is.TypeOf<Ok>());
+        }
+    }
+}

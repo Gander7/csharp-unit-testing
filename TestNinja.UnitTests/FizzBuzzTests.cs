3ec9af3f954eeae8393b0d5fbf80a67710d90b5a
Zach Allen <zachjallen@gmail.com>
2019-06-12 16:44:47 -0300

    added FizzBuzz exercise including tests

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_DivisibleBy5And3_ReturnsFizzBuzz()
+        {
+            var actual = FizzBuzz.GetOutput(15);
+
+            Assert.That(actual, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_DivisibleBy3_ReturnsFizz()
+        {
+            var actual = FizzBuzz.GetOutput(9);
+
+            Assert.That(actual, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_DivisibleBy5_ReturnsBuzz()
+        {
+            var actual = FizzBuzz.GetOutput(10);
+
+            Assert.That(actual, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_NotDivisibleBy3Or5_ReturnsNumberArg()
+        {
+            var actual = FizzBuzz.GetOutput(7);
+
+            Assert.That(actual, Is.EqualTo(7.ToString()));
+        }
+    }
+}

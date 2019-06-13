6ca26122b5e813db2f37f2637d29e6c43fb03a3c
Zach Allen <zachjallen@gmail.com>
2019-06-13 15:23:57 -0300

    added interaction tests for OrderService

---
+++ TestNinja/Mocking/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.Mocking
+{
+    public class OrderService
+    {
+        private readonly IStorage _storage;
+
+        public OrderService(IStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public int PlaceOrder(Order order)
+        {
+            var orderId = _storage.Store(order);
+
+            // other work
+
+            return orderId;
+        }
+    }
+
+    public class Order
+    {
+
+    }
+
+    public interface IStorage
+    {
+        int Store(Order order);
+    }
+}

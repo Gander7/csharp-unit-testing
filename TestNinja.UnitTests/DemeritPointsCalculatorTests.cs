c488228cc769d13007053a0d6e8d737e7d7fde4d
Zach Allen <zachjallen@gmail.com>
2019-06-13 08:50:23 -0300

    added demerit points calculator exercise with tests

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _calculator;
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowsArgumentOutOfRangeException(int speed)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.CalculateDemeritPoints(speed));
+            // or
+            //Assert.That(() => _calculator.CalculateDemeritPoints(-1),
+            //    Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0,0)]
+        [TestCase(1, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnsDemeritPoints(int speed, int expected)
+        {
+            var actual = _calculator.CalculateDemeritPoints(speed);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ TestNinja/DemeritPointsCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja
+{
+    public class DemeritPointsCalculator
+    {
+        private const int SpeedLimit = 65;
+        private const int MaxSpeed = 300;
+        private const int MinSpeed = 0;
+
+        public int CalculateDemeritPoints(int speed)
+        {
+            if (speed < MinSpeed || speed > MaxSpeed)
+                throw new ArgumentOutOfRangeException();
+            if (speed <= SpeedLimit)
+                return 0;
+
+            const int kmPerDemeritPoints = 5;
+            var demeritPoints = (speed - SpeedLimit) / kmPerDemeritPoints;
+
+            return demeritPoints;
+        }
+    }
+}
